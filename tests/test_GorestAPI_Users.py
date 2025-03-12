c71f8b41eb22a069c65cf38cf613663158a6684c
ManjunathHKShetty <manjunathhkshetty@gmail.com>
2025-03-12 13:07:52 +0530

    GoRest API

--- tests/test_GorestAPI_Users.py
+++ tests/test_GorestAPI_Users.py
@@ -2,25 +2,29 @@ import json
 import pytest
 import allure
 from utils.apis import APIs
+from utils.logger import LogGen
 from utils.helpers import generate_random_email
+from utils.libs import assert_status_code, assert_response_contains_field, assert_field_value
+
+logger = LogGen.loggen()
 
 @pytest.fixture(scope="module")
 def apis(auth_token):
     return APIs(auth_token)
 
-
 @allure.severity(allure.severity_level.NORMAL)
 def test_get_users(apis):
+    logger.info("GETTING USERS......")
     response = apis.get("/public/v2/users")
-    assert response.status_code == 200, f"Expected 200, got {response.status_code}"
+    assert_status_code(response, 200)
     json_data = response.json()
     json_str = json.dumps(json_data, indent=4)
-    print("json GET response body:", json_str)
-    print("GET USER IS DONE.....")
-
+    logger.info(f"json GET response body: {json_str}")
+    logger.info("GET USER IS DONE.....")
 
 @allure.severity(allure.severity_level.NORMAL)
 def test_post_user(apis):
+    logger.info("ADDING THE USER......")
     user_data = {
         "name": "John Automation",
         "email": generate_random_email(),
@@ -28,15 +32,15 @@ def test_post_user(apis):
         "status": "active"
     }
     response = apis.post("/public/v2/users/", user_data)
+    assert_status_code(response, 201)
     json_data = response.json()
     json_str = json.dumps(json_data, indent=4)
-    print("json POST response body:", json_str)
-    assert response.status_code == 201
+    logger.info(f"json GET response body: {json_str}")
     user_id = json_data["id"]
-    print("user id ===>> ", user_id)
-    assert 'name' in json_data
-    assert json_data['name'] == "John Automation"
-    print("POST USER IS DONE.....")
+    logger.info(f"user id ===>> {user_id}")
+    assert_response_contains_field(json_data, 'name')
+    assert_field_value(json_data, 'name', "John Automation")
+    logger.info("POST USER IS DONE.....")
 
 @pytest.fixture(scope="module")
 def user_id(apis):
@@ -52,26 +56,27 @@ def user_id(apis):
 
 @allure.severity(allure.severity_level.NORMAL)
 def test_put_user(apis, user_id):
+    logger.info("UPDATING THE USER......")
     user_data = {
-    "name": "John Automation Labs",
-    "email": generate_random_email(),
-    "gender": "male",
-    "status": "inactive"
-}
+        "name": "John Automation Labs",
+        "email": generate_random_email(),
+        "gender": "male",
+        "status": "inactive"
+    }
     response = apis.put(f"/public/v2/users/{user_id}", user_data)
-    assert response.status_code == 200
+    assert_status_code(response, 200)
     json_data = response.json()
     json_str = json.dumps(json_data, indent=4)
-    print("json PUT response body:", json_str)
-    assert json_data["id"] == user_id
-    assert json_data["name"] == "John Automation Labs"
-    assert json_data["gender"] == "male"
-    print("UPDATE USER IS DONE.....")
-    print("....=====================....")
+    logger.info(f"json GET response body: {json_str}")
+    assert_field_value(json_data, "id", user_id)
+    assert_field_value(json_data, "name", "John Automation Labs")
+    assert_field_value(json_data, "gender", "male")
+    logger.info("UPDATE USER IS DONE.....")
 
 @allure.severity(allure.severity_level.NORMAL)
 def test_delete_user(apis, user_id):
+    logger.info("DELETING THE USER.....")
     response = apis.delete(f"/public/v2/users/{user_id}")
-    assert response.status_code == 204
-    print("DELETE USER IS DONE.....")
-    print("END OF API TESTING")
+    assert_status_code(response, 204)
+    logger.info("DELETE USER IS DONE.....")
+    logger.info("END OF API TESTING")
